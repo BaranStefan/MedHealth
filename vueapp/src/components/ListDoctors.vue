85d89d4f8348f33833eab490b6d7a48c8feb791e
Stefan <baranstefanoctavian@gmail.com>
2023-08-24 18:04:57 +0300

    Separate pages for adding and listing doctors

--- vueapp/src/components/ListDoctors.vue
+++ vueapp/src/components/ListDoctors.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <!-- Listing Doctors -->
-        <div v-for="doctor in doctors" :key="doctor.id">
+        <div class="doctor-item" v-for="doctor in doctors" :key="doctor.id">
             <h3>{{ doctor.name }} - {{ doctor.speciality }}</h3>
             <p>Phone: {{ doctor.phone }}</p>
             <p>Email: {{ doctor.mail }}</p>
@@ -9,15 +9,8 @@
             <button @click="deleteDoctor(doctor.id)">Delete</button>
         </div>
 
-        <!-- Form to Add/Edit Doctor -->
-        <div class="form-container">
-            <input v-model="currentDoctor.name" placeholder="Doctor Name" />
-            <input v-model="currentDoctor.speciality" placeholder="Speciality" />
-            <input v-model="currentDoctor.phone" placeholder="Phone" />
-            <input v-model="currentDoctor.mail" type="email" placeholder="Email" />
-            <button v-if="!currentDoctor.id" @click="createDoctor">Add</button>
-            <button v-if="currentDoctor.id" @click="updateDoctor">Update</button>
-        </div>
+        <!-- Button to navigate to Add Doctor page -->
+        <button @click="goToAddDoctor">Add Doctor</button>
     </div>
 </template>
 
@@ -27,8 +20,7 @@
     export default {
         data() {
             return {
-                doctors: [],
-                currentDoctor: {}
+                doctors: []
             };
         },
         async created() {
@@ -39,21 +31,12 @@
                 const response = await axios.get("/api/doctors");
                 this.doctors = response.data;
             },
-            editDoctor(doctor) {
-                this.currentDoctor = { ...doctor };
-            },
-            async createDoctor() {
-                await axios.post("/api/doctors", this.currentDoctor);
-                await this.fetchDoctors();
-            },
-            async updateDoctor() {
-                await axios.put(`/api/doctors/${this.currentDoctor.id}`, this.currentDoctor);
-                await this.fetchDoctors();
-                this.currentDoctor = {};
-            },
             async deleteDoctor(id) {
                 await axios.delete(`/api/doctors/${id}`);
                 await this.fetchDoctors();
+            },
+            goToAddDoctor() {
+                this.$router.push('/add-doctor');
             }
         }
     };
@@ -75,6 +58,12 @@
         gap: 10px; /* provides spacing between form elements */
         width: 300px;
         margin-top: 20px;
+    }
+
+    .doctor-item {
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 10px;
+        margin-bottom: 10px;
     }
 
     input, button {
--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -1,24 +1,23 @@
-// main.js
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/HomePage.vue';
 import Login from './components/Login.vue';
 import Register from './components/Register.vue';
-import Doctors from '@/components/Doctors.vue';
-import Appointment from '@/components/Appointment.vue';
+import ListDoctors from '@/components/ListDoctors.vue';  // import the ListDoctors component
+import AddDoctor from '@/components/AddDoctor.vue';  // import the AddDoctor component
+import Appointments from '@/components/Appointments.vue';  // import the Appointments component
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://localhost:7053';
-
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
-    { path: '/doctors', component: Doctors },
-    { path: '/appointment/:doctorId', component: Appointment, props: true }
+    { path: '/doctors', component: ListDoctors },  // Path for ListDoctors component
+    { path: '/add-doctor', component: AddDoctor },  // Path for AddDoctor component
+    { path: '/appointments', component: Appointments },  // New path for Appointments component
 ];
 
 const router = createRouter({
