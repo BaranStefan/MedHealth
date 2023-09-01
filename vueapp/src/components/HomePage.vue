be9b570524f43b8f13d5f54716ff650b5b3fab9f
Stefan <baranstefanoctavian@gmail.com>
2023-09-01 21:41:24 +0300

    Home page update

--- vueapp/src/components/HomePage.vue
+++ vueapp/src/components/HomePage.vue
@@ -1,16 +1,15 @@
 <template>
-    <div>
-        <h2>Wellcome to our Medical cabinet</h2>
+    <div class="container">
+        <h2>Welcome to our Medical Cabinet</h2>
         <p>Already have an account? <router-link to="/login">Login</router-link></p>
         <p>Don't have an account?  <router-link to="/register">Register</router-link></p>
-        <p>  <router-link to="/doctors">See Doctors</router-link></p>
-        <p>  <router-link to="/appointments">Book an Appointment</router-link></p>
+        <p><router-link to="/doctors">See Doctors</router-link></p>
+        <p><router-link to="/appointments">Book an Appointment</router-link></p>
     </div>
 </template>
 
 <script>
     export default {
-        
         data() {
             return {
                 // Your data properties...
@@ -23,5 +22,35 @@
 </script>
 
 <style scoped>
-    /* Your styles... */
+    
+    .container {
+        width: 100%;
+        max-width: 600px; 
+        margin: 50px auto; 
+        text-align: center; 
+        font-family: 'Arial', sans-serif;
+    }
+
+    
+    h2 {
+        font-size: 2em;
+        margin-bottom: 20px; 
+        color: #333; 
+    }
+
+    
+    p {
+        font-size: 1.2em;
+        margin: 10px 0; 
+    }
+
+   
+    router-link {
+        color: #007BFF; 
+        text-decoration: none;
+    }
+
+        router-link:hover {
+            text-decoration: underline;
+        }
 </style>
